0bc9ec313a3e3c83becbf00c81416af426d2d4ae
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-04-12 01:27:21 +0530

    Plotting Graph

--- Visibility Graph.py
+++ Visibility Graph.py
@@ -5,3 +5,14 @@ Created on Tue Apr 12 01:21:53 2022
 @author: satvi
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from visibility_graph import visibility_graph
+
+ds_btc = pd.read_csv('Q:/Non Linear Dynamics/Visibility Graph/Stock Prices/BTC-USD 1-01-2020 -- 31-12-2021.csv')
+
+btc_plot_close = ds_btc[['Date', 'Close']]
+
+btc_plot_close.plot()
